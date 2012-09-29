2cb8e380bdffdea9d2967f9eff67a72756020684
Naohisa Takahashi <hakuro16@gmail.com>
2012-09-29 23:27:21 +0900

    enable json response

--- src/main/java/jp/dip/hakuro/twitserif/web/SerifsController.java
+++ src/main/java/jp/dip/hakuro/twitserif/web/SerifsController.java
@@ -3,11 +3,31 @@ package jp.dip.hakuro.twitserif.web;
 import jp.dip.hakuro.twitserif.domain.Serifs;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
 
 @RequestMapping("/serifses")
 @Controller
 @RooWebScaffold(path = "serifses", formBackingObject = Serifs.class)
 public class SerifsController {
-//    public 
+
+    @RequestMapping(value = "/addentry", method = RequestMethod.POST, produces = "text/html")
+    public String addEntry(@Valid Serifs serifs, BindingResult bindingResult,
+            HttpServletRequest httpServletRequest) {
+        return "serifses/list";
+    }
+
+    @RequestMapping(value = "/getrecent/{number}", headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> getRecent(@PathVariable String number) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+
+        List<Serifs> result = Serifs.findAllSerifses();
+        return new ResponseEntity<String>(Serifs.toJsonArray(result), headers, HttpStatus.OK);
+    }
 }
